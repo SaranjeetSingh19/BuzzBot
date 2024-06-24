74b43f1aaa3839456afdb3605f007fabb5ff35d6
SaranjeetSingh19 <ss5259603@gmail.com>
2024-06-24 16:36:08 +0530

    User routes & controllers

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import mongoDbConnection from "./db/mongoDb.js";
 import cookieParser from "cookie-parser";
 
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true })); // to parse body data
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT} PORT 🚀`);
