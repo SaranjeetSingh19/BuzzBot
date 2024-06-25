7f753abe8eb30192b8017a6aa3038a11e6cf807a
SaranjeetSingh19 <ss5259603@gmail.com>
2024-06-25 11:50:35 +0530

    Notification routes & controllers

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
+
+
 import mongoDbConnection from "./db/mongoDb.js";
 import cookieParser from "cookie-parser";
 
@@ -19,6 +22,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT} PORT 🚀`);
