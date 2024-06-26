32505c1812fbd119b5ddf6127b5892597771b6dd
SaranjeetSingh19 <ss5259603@gmail.com>
2024-06-26 11:13:04 +0530

    (UI) Home page & Sidebar

---
+++ frontend/src/components/common/RightPanel.jsx
@@ -0,0 +1,65 @@
+import { Link } from "react-router-dom";
+import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
+import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
+
+const RightPanel = () => {
+  const isLoading = false;
+
+  return (
+    <div className="hidden lg:block my-4 mx-2">
+      <div className="bg-[#16181C] p-4 rounded-md sticky top-2">
+        <p className="font-bold">Who to follow</p>
+        <div className="flex flex-col gap-4">
+          {/* item */}
+          {isLoading && (
+            <>
+              <RightPanelSkeleton />
+              <RightPanelSkeleton />
+              <RightPanelSkeleton />
+              <RightPanelSkeleton />
+            </>
+          )}
+          {!isLoading &&
+            USERS_FOR_RIGHT_PANEL?.map((user) => (
+              <Link
+                to={`/profile/${user.username}`}
+                className="flex items-center justify-between gap-4"
+                key={user._id}
+              >
+                <div className="flex gap-2 items-center">
+                  <div className="avatar">
+                    <div className="w-8 rounded-full">
+                      <img
+                        src={
+                          "https://imgs.search.brave.com/EwOyFX85KluR1etw_SpvA3ho6qt3yzZpaOkhW-aemnU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9waG90/b3Nub3cub3JnL3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDI0LzA0/L25vYml0YS1waG90/b18xNi5qcGc" ||
+                          user.profileImg ||
+                          "/avatar-placeholder.png"
+                        }
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="font-semibold tracking-tight truncate w-28">
+                      {user.fullName}
+                    </span>
+                    <span className="text-sm text-slate-500">
+                      @{user.username}
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  <button
+                    className="btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm"
+                    onClick={(e) => e.preventDefault()}
+                  >
+                    Follow
+                  </button>
+                </div>
+              </Link>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default RightPanel;
